998da0bd65f9476ad1d01877fa9337cd539c1ac3
Jon Ackley <jonackley16@gmail.com>
2022-11-18 09:09:40 -0500

    homepage up, little css

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+    return (
+        <div className="nav-bar">
+            <button className="button">Friendly Sushi</button>
+            <button className="button">All the Sushi</button>
+            <button className="button">Your Sushi</button>
+            <button className="button">Log In</button>
+            <button className="button">Log Out</button>
+        </div>
+    )
+}
