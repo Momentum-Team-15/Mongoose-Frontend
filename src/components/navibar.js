44dcab55654315fbe3f73533e2416b045de6efbf
Lucian Lodico <llodico@mac.com>
2022-11-19 14:04:34 -0500

    mock, start pages

---
+++ src/components/navibar.js
@@ -0,0 +1,10 @@
+export const NavBar = () => {
+    return (
+        <div className="nav-bar">
+            <button className="button-1">My Rolls</button>
+            <button className="button-1">My Friends' Rolls</button>
+            <button className="button-1">Liked Rolls</button>
+            <button className="button-1">Rolls by Other Chefs</button>
+      </div>
+    )
+}
